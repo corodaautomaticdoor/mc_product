f63ae1279449fdd47f70fe33de4db82a3215830e
Gary <garychavezj@gmail.com>
2020-07-20 11:41:19 -0500

    feat : implementa logica de la capa entity

--- src/main/java/com/coroda/entity/DetailProduct.java
+++ src/main/java/com/coroda/entity/DetailProduct.java
@@ -1,5 +1,31 @@
 package com.coroda.entity;
 
+import lombok.Data;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "detail_product")
 public class DetailProduct {
 
+    @Column(name = "id")
+    private Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "detailId")
+    @Column(name = "detailId")
+    private Long detailId;
+
+    @Column(name = "material")
+    private String material;
+
+    @Column(name = "dimensions")
+    private String dimensions;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "priceUnit")
+    private BigDecimal priceUnit;
 }
--- src/main/java/com/coroda/entity/Product.java
+++ src/main/java/com/coroda/entity/Product.java
@@ -1,5 +1,36 @@
 package com.coroda.entity;
 
+import lombok.Data;
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "id")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "origin")
+    private OriginProduct origin;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "id")
+    private List<DetailProduct> detail;
 
 }
