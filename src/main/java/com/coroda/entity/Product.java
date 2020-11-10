1bbe733319d6e473b59952caf404852ef692d09f
Gary Alberto Chavez Jimenez <gchavezj@everis.com>
2020-11-10 01:19:49 -0500

    fix : actualiza capa entity

--- src/main/java/com/coroda/entity/Product.java
+++ src/main/java/com/coroda/entity/Product.java
@@ -2,6 +2,7 @@ package com.coroda.entity;
 
 import lombok.Data;
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -12,25 +13,45 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "id")
     @Column(name = "id")
+    //identificador
     private Long id;
 
+    @Column(name = "category")
+    //puerta,equipo de seguridad ,motores ,sistemas
+    private String category;
+
+    @Column(name = "subCategory")
+    //puerta garage, puer..  ,cercoelectrico , etc
+    private String subCategory;
+
     @Column(name = "model")
+    //PG01 ,PC01 ,...
     private String model;
 
     @Column(name = "brand")
+    //marca
     private String brand;
-
-    @Column(name = "category")
-    private String category;
 
     @Column(name = "description")
     private String description;
 
     @Column(name = "origin")
+    //nacional , importado
     private OriginProduct origin;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "id")
-    private List<DetailProduct> detail;
+    @Column(name = "material")
+    //madera , metal
+    private String material;
 
+    @Column(name = "dimensions")
+    private String dimensions;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "priceUnit")
+    private BigDecimal priceUnit;
+
+    @Column(name = "image")
+    private String image;
 }
